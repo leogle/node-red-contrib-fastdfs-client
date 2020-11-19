36c78c192db8915d1e02c8b0f5f18b9b45ff1922
liurh <leogle@qq.com>
2020-11-19 10:23:18 +0800

    第一次提交

---
+++ src/fastdfs-download.js
@@ -0,0 +1,29 @@
+module.exports = function (RED) {
+    function FastDfsDownNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.tracker = RED.nodes.getNode(config.tracker);
+        
+        node.on('input', function (msg) {
+            let FdfsClient = require('fastdfs-client');
+            let fdfs = new FdfsClient({
+                trackers: [{
+                    host: this.tracker.host,
+                    port: this.tracker.port
+                }],
+                //timeout: this.tracker.timeout,
+                charset: this.tracker.charset
+            });
+            let fileId = config.fileid || msg.fileId;
+            let fileName = config.filename || msg.fileName;
+            fdfs.download(fileId,fileName).then(function () {
+                node.send(msg);
+            }).catch(function (err) {
+                console.error(err);
+                node.send(msg);
+            });
+
+        });
+    }
+    RED.nodes.registerType("fastdfs-download", FastDfsDownNode);
+}
---
+++ src/fastdfs-save.js
@@ -0,0 +1,43 @@
+module.exports = function (RED) {
+    //tracker 配置节点
+    function TrackerNode(config) {
+        RED.nodes.createNode(this, config);
+        this.host = config.host;
+        this.port = config.port;
+        this.timeout = config.timeout;
+        this.charset = config.charset;
+    }
+
+    function FastDfsSaveNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.tracker = RED.nodes.getNode(config.tracker);
+        node.on('input', function (msg) {
+            let FdfsClient = require('fastdfs-client');
+            let fdfs = new FdfsClient({
+                trackers: [{
+                    host: this.tracker.host,
+                    port: this.tracker.port
+                }],
+                //timeout: this.tracker.timeout,
+                charset: this.tracker.charset
+            });
+            let fileName = config.filename || msg.filename
+            fdfs.upload(fileName, {
+                //     // 上传方法 [upload, uploadAppender, append, modify], 默认为upload
+                method: 'upload',
+                // 指定文件存储的group，不指定则由tracker server分配
+                group: 'group1',
+            }).then(function (fileId) {
+                msg.payload=fileId;
+                node.send(msg);
+            }).catch(function (err) {
+                console.error(err);
+                node.send(msg);
+            });
+
+        });
+    }
+    RED.nodes.registerType("fastdfs-tracker", TrackerNode);
+    RED.nodes.registerType("fastdfs-save", FastDfsSaveNode);
+}
